803a9d48224672bf7f0d4ca03c60056109965b7c
Anch0rman <johnblankemeier@gmail.com>
2024-06-05 18:16:15 -0400

    Update the models

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,20 @@
 
 namespace App\Models;
 
-use \Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 
-class Job extends Model {
+class Job extends Model
+{
+    use HasFactory, Notifiable;
+
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
